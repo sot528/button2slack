ac74c9a500f3d67b3adb2398f4c76605ab43a8ee
sot528 <sot528@gmail.com>
2019-04-17 12:56:03 +0900

    fix payload

--- handler.py
+++ handler.py
@@ -14,34 +14,13 @@ def handler(event, context):
         'username': payload['username'] if 'username' in payload is None else u'alis',
         'icon_emoji': payload['icon_emoji'] if 'icon_emoji' in payload is None else u':alischan:',
         'link_names': 1,  # メンションを有効にする
-        'attachments': payload['attachments']
+        'text': os.environ['MESSAGE']
     }))
 
-    return self.event
+    return True
 
 
 def create_payload():
-    attachment = {
-        "fallback": "トークン配布通知",
-
-        "text": "トークン配布結果",
-        "pretext": "",
-
-        "color": "#36a64f",
-
-        "fields": [
-            {
-                "title": ":alis: トークン配布が完了しました :alis:",
-                "value":
-                    "配布記事数: "
-                    + "`\n 配布ユーザ数(いいね): ",
-                "short": False
-            }
-        ]
-    }
-
-    payload = {
-        'attachments': [attachment]
-    }
+    payload = {}
 
     return payload
