d56b21ea75df046520446ff912e89c1eaffc9ec3
sot528 <sot528@gmail.com>
2020-05-18 19:48:51 +0900

    edit

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 1. Get [IoT Enterprise Button](https://www.amazon.co.jp/gp/aw/d/B075FPHHGG/)
 1. [Setting the Button with App](https://docs.aws.amazon.com/ja_jp/iot-1-click/latest/developerguide/1click-mobile-app.html)
 1. Get Button Device ID
-1. Set Device ID env  
+1. Set Device ID to env  
    `direnv edit`
-1. `./deploy.sh`
+1. `npm i`
+1. `npm run deploy`
 1. Create IoT 1click project & setting created Lambda to the device.
 1. Push the button
    
--- src/handler.py
+++ src/handler.py
@@ -12,7 +12,7 @@ def handler(event, context):
 
     requests.post(os.environ['SLACK_NOTIFICATION_URL'], data=json.dumps({
         'username': payload['username'] if 'username' in payload is None else u'alis',
-        'icon_emoji': payload['icon_emoji'] if 'icon_emoji' in payload is None else u':alischan:',
+        'icon_emoji': payload['icon_emoji'] if 'icon_emoji' in payload is None else u':baby:',
         'link_names': 1,  # メンションを有効にする
         'text': os.environ['MESSAGE']
     }))
